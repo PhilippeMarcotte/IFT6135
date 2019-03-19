Fixed GRU computation

HW2/models.py
@@ -238,7 +238,9 @@ class GRUCell(nn.Module):
 
         self.W = nn.Parameter(torch.zeros((input_size, 3 * hidden_size)))
         self.U = nn.Parameter(torch.zeros((hidden_size, 3 * hidden_size)))
-        self.b = nn.Parameter(torch.zeros((3 * hidden_size)))
+        self.br = nn.Parameter(torch.zeros(hidden_size))
+        self.bz = nn.Parameter(torch.zeros(hidden_size))
+        self.bh_tilde = nn.Parameter(torch.zeros(hidden_size))
 
         self.reset_parameters()
 
@@ -246,20 +248,22 @@ class GRUCell(nn.Module):
         bound = 1/np.sqrt(self.hidden_size)
         nn.init.uniform_(self.W, -bound, bound)
         nn.init.uniform_(self.U, -bound, bound)
-        nn.init.uniform_(self.b, -bound, bound)
+        nn.init.uniform_(self.br, -bound, bound)
+        nn.init.uniform_(self.bz, -bound, bound)
+        nn.init.uniform_(self.bh_tilde, -bound, bound)
 
     def forward(self, input, hidden):
-        gate_x = torch.matmul(input, self.W).add(self.b)
-        gate_h = torch.matmul(hidden, self.U) + self.b
+        gate_x = torch.matmul(input, self.W)
+        gate_h = torch.matmul(hidden, self.U)
 
-        i_r, i_i, i_n = gate_x.chunk(3, 1)
-        h_r, h_i, h_n = gate_h.chunk(3, 1)
+        i_r, i_z, i_n = gate_x.chunk(3, 1)
+        h_r, h_z, h_n = gate_h.chunk(3, 1)
 
-        resetgate = torch.sigmoid(i_r + h_r)
-        inputgate = torch.sigmoid(i_i + h_i)
-        newgate = torch.tanh(i_n + (resetgate * h_n))
+        r = torch.sigmoid(i_r + h_r + self.br)
+        z = torch.sigmoid(i_z + h_z + self.bz)
+        h_tilde = torch.tanh(i_n + (r * h_n) + self.bh_tilde)
 
-        h = newgate + inputgate * (hidden - newgate)
+        h = (1 - z) * hidden + z * h_tilde
         return h
 
 