6179dea8e896b398463650065c1b17904f528628
Unknown <olibd@outlook.com>
2019-04-14 21:29:46 -0400

    VAE Fix code

---
+++ HW3/Utils.py
@@ -0,0 +1,14 @@
+import torch
+
+
+def save_model(model, optimizer, epoch, trainingLoss, validationLoss):
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'trainingLoss': trainingLoss,
+        'validationLoss': validationLoss,
+    }, "checkpoint.pth")
+
+def load_model():
+    return torch.load("checkpoint.pth")
---
+++ HW3/VAE.py
@@ -0,0 +1,141 @@
+import math
+
+import numpy as np
+import torch
+from torch import nn, cuda
+from torch.optim import Adam
+
+from Utils import save_model
+from mnist_loader import get_data_loader
+
+class Squeeze(nn.Module):
+    def __init__(self, *args):
+        super(Squeeze, self).__init__()
+        self.shape = args
+
+    def forward(self, x):
+        return x.squeeze()
+
+class Unsqueeze(nn.Module):
+    def __init__(self, *args):
+        super(Unsqueeze, self).__init__()
+        self.shape = args
+
+    def forward(self, x):
+        return x.view(-1, 256, 1, 1)
+
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=(3, 3)),
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=(3, 3)),
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 256, kernel_size=(5, 5)),
+            nn.ELU(),
+            Squeeze(),
+            nn.Linear(in_features=256, out_features=100*2)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(in_features=100, out_features=256),
+            Unsqueeze(),
+            nn.ELU(),
+            nn.Conv2d(256, 64, kernel_size=(5, 5), padding=(4,4)),
+            nn.ELU(),
+            nn.UpsamplingBilinear2d(scale_factor=2),
+            nn.Conv2d(64, 32, kernel_size=(3,3), padding=(2,2)),
+            nn.ELU(),
+            nn.UpsamplingBilinear2d(scale_factor=2),
+            nn.Conv2d(32, 16, kernel_size=(3,3), padding=(2,2)),
+            nn.ELU(),
+            nn.Conv2d(16, 1, kernel_size=(3,3), padding=(2,2)),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        q_params = self.encoder(x)
+        mu = q_params[:, 100:]
+        log_sigma = q_params[:, :100]
+        z = self.reparameterize(mu, log_sigma)
+        return self.decoder(z), mu, log_sigma
+
+    def reparameterize(self, mu, log_sigma):
+        sigma = torch.exp(log_sigma) + math.exp(-7)
+
+        e = torch.randn_like(sigma)
+        return mu + sigma * e
+
+
+def ELBOWLoss(x, x_, mu, log_sigma):
+    KL = KLDivergence(mu, log_sigma)
+
+    BCE = nn.BCELoss()
+    logpx_z = BCE(x_, x)
+    return -(logpx_z - KL) #batch mean loss computed in BCE
+
+def KLDivergence(mu, log_sigma):
+    return -0.5 * torch.sum(1 + log_sigma - mu.pow(2) - log_sigma.exp());
+
+
+def train(VAE, train_loader, optimizer, device):
+    VAE.train()
+    losses = []
+
+    for batch_id, batchX in enumerate(train_loader):
+        batchX = batchX.to(device)
+        optimizer.zero_grad()
+        batchX_, mu, log_sigma = VAE(batchX)
+        loss = ELBOWLoss(batchX, batchX_, mu, log_sigma)
+        loss.backward()
+        losses.append(loss.item())
+        optimizer.step()
+
+        print("------ Train batch " + str(batch_id) + " ------")
+        print("batch loss: " + str(loss))
+
+    return np.mean(losses)
+
+
+def validate(VAE, validation_loader, device):
+    VAE.eval()
+    losses = []
+
+    with torch.no_grad():
+        for batch_id, batchX in enumerate(train_loader):
+            batchX = batchX.to(device)
+            batchX_, mu, log_sigma = VAE(batchX)
+            loss = ELBOWLoss(batchX, batchX_, mu, log_sigma)
+            losses.append(loss.item())
+
+            print("------ Validation batch " + str(batch_id) + " ------")
+            print("batch loss: " + str(loss))
+
+    return np.mean(losses)
+
+
+if __name__ == "__main__":
+
+    device = torch.device("cpu")
+    if cuda.is_available():
+        device = torch.device("cuda")
+
+    train_loader, valid_loader, test_loader = get_data_loader("binarized_mnist", 64)
+
+    VAE = VAE()
+    VAE.to(device)
+    optimizer = Adam(params=VAE.parameters(), lr=3*10**(-4))
+
+    for epoch in range(20):
+        print("----------------Epoch #" + str(epoch) + "----------------")
+
+        trainLoss = train(VAE, train_loader, optimizer, device)
+        print("Epoch train loss" + str(trainLoss))
+
+        validationLoss = validate(VAE, valid_loader, device)
+        print("Epoch validation loss" + str(validationLoss))
+
+        save_model(VAE, optimizer, epoch, trainLoss, validationLoss)
