cf4cdc9b3111523eb886d59e81ad6fb63793f4d8
mikperr <44344219+mikperr@users.noreply.github.com>
2019-04-16 16:48:06 -0400

    Create wgangp.py

---
+++ HW3/wgangp.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 16:53:56 2019
+
+@author: mikap
+"""
+import os
+import numpy as np
+import math
+import sys
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd as autograd
+import torch
+import torchvision.datasets
+from torch.utils.data import dataset
+from torch import nn
+# from torch.nn.modules import upsampling
+# from torch.functional import F
+from torch.optim import Adam
+
+os.makedirs("images", exist_ok=True)
+
+
+img_shape = (3,32,32)
+
+cuda = True if torch.cuda.is_available() else False
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        self.gen=nn.Sequential(
+                nn.Linear(100,100),
+                nn.ReLU(True),
+                nn.Linear(100,256),
+                nn.ReLU(True),
+                nn.Linear(256,512),
+                nn.ReLU(True),
+                nn.Linear(512,1024)
+                
+                
+                )
+
+    def forward(self, z):
+        img = self.gen(z)
+        img = img.view(img.shape[0], 3, 32,32)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+                nn.Linear(1024,512),
+                nn.ReLU(True),
+                nn.Linear(512,256),
+                nn.ReLU(True),
+                nn.Linear(256,100),
+                nn.ReLU(True),
+                nn.Linear(100,100),
+                
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.shape[0], -1)
+        validity = self.gen(img_flat)
+        return validity
+
+
+# Loss weight for gradient penalty
+lambda_gp = 10
+
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
+
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+
+# Configure data loader
+image_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((.5, .5, .5),
+                         (.5, .5, .5))
+])
+
+
+def get_data_loader(dataset_location, batch_size):
+    trainvalid = torchvision.datasets.SVHN(
+        dataset_location, split='train',
+        download=True,
+        transform=image_transform
+    )
+
+    trainset_size = int(len(trainvalid) * 0.9)
+    trainset, validset = dataset.random_split(
+        trainvalid,
+        [trainset_size, len(trainvalid) - trainset_size]
+    )
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=2
+    )
+
+    validloader = torch.utils.data.DataLoader(
+        validset,
+        batch_size=batch_size,
+    )
+
+    testloader = torch.utils.data.DataLoader(
+        torchvision.datasets.SVHN(
+            dataset_location, split='test',
+            download=True,
+            transform=image_transform
+        ),
+        batch_size=batch_size,
+    )
+
+    return trainloader, validloader, testloader
+
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5,0.999))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+
+def compute_gradient_penalty(D, real_samples, fake_samples):
+    """Calculates the gradient penalty loss for WGAN GP"""
+    # Random weight term for interpolation between real and fake samples
+    alpha = Tensor(np.random.random((real_samples.size(0), 1, 1, 1)))
+    # Get random interpolation between real and fake samples
+    interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
+    d_interpolates = D(interpolates)
+    fake = Variable(Tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False)
+    # Get gradient w.r.t. interpolates
+    gradients = autograd.grad(
+        outputs=d_interpolates,
+        inputs=interpolates,
+        grad_outputs=fake,
+        create_graph=True,
+        retain_graph=True,
+        only_inputs=True,
+    )[0]
+    gradients = gradients.view(gradients.size(0), -1)
+    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+    return gradient_penalty
+
+
+# ----------
+#  Training
+# ----------
+n_epochs=50
+n_critic=5
+latent_dim=100
+sample_interval=400
+train, valid, test = get_data_loader("svhn", 32)
+batches_done = 0
+for epoch in range(n_epochs):
+    for i, (imgs, _) in enumerate(train):
+
+        # Configure input
+        real_imgs = Variable(imgs.type(Tensor))
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Sample noise as generator input
+        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], latent_dim))))
+
+        # Generate a batch of images
+        fake_imgs = generator(z)
+
+        # Real images
+        real_validity = discriminator(real_imgs)
+        # Fake images
+        fake_validity = discriminator(fake_imgs)
+        # Gradient penalty
+        gradient_penalty = compute_gradient_penalty(discriminator, real_imgs.data, fake_imgs.data)
+        # Adversarial loss
+        d_loss = -torch.mean(real_validity) + torch.mean(fake_validity) + lambda_gp * gradient_penalty
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        optimizer_G.zero_grad()
+
+        # Train the generator every n_critic steps
+        if i % n_critic == 0:
+
+            # -----------------
+            #  Train Generator
+            # -----------------
+
+            # Generate a batch of images
+            fake_imgs = generator(z)
+            # Loss measures generator's ability to fool the discriminator
+            # Train on fake images
+            fake_validity = discriminator(fake_imgs)
+            g_loss = -torch.mean(fake_validity)
+
+            g_loss.backward()
+            optimizer_G.step()
+
+            print(
+                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                % (epoch, n_epochs, i, len(train), d_loss.item(), g_loss.item())
+            )
+            
+            if batches_done % sample_interval == 0:
+                save_image(fake_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+
+            batches_done += n_critic
