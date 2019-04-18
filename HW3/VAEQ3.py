0eee2288cb6f8992b6c5f54e8299bf53dedff2ce
mikperr <44344219+mikperr@users.noreply.github.com>
2019-04-18 17:57:17 -0400

    Files for VAE Q3/Separate train routine for GAN

---
+++ HW3/VAEQ3.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 17:02:12 2019
+
+@author: mikap
+"""
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch import nn, cuda
+from torch.optim import Adam
+
+from Utils import save_model
+import os
+import sys
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.autograd as autograd
+import torchvision.datasets
+from torch.utils.data import dataset
+# from torch.nn.modules import upsampling
+# from torch.functional import F
+
+class Squeeze(nn.Module):
+    def __init__(self, *args):
+        super(Squeeze, self).__init__()
+        self.shape = args
+
+    def forward(self, x):
+        return x.squeeze()
+
+class Unsqueeze(nn.Module):
+    def __init__(self, *args):
+        super(Unsqueeze, self).__init__()
+        self.shape = args
+
+    def forward(self, x):
+        return x.view(-1, 256, 1, 1)
+
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=(5, 5)),
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=(5, 5)),
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 256, kernel_size=(5, 5)),
+            nn.ELU(),
+            Squeeze(),
+            nn.Linear(in_features=256, out_features=100*2)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(in_features=100, out_features=256),
+            Unsqueeze(),
+            nn.ELU(),
+            nn.Conv2d(256, 64, kernel_size=(5, 5), padding=(4,4)),
+            nn.ELU(),
+            nn.UpsamplingBilinear2d(scale_factor=2),
+            nn.Conv2d(64, 32, kernel_size=(3,3), padding=(2,2)),
+            nn.ELU(),
+            nn.UpsamplingBilinear2d(scale_factor=2),
+            nn.Conv2d(32, 16, kernel_size=(3,3), padding=(3,3)),
+            nn.ELU(),
+            nn.Conv2d(16, 3, kernel_size=(3,3), padding=(3,3)),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        q_params = self.encoder(x)
+        mu = q_params[:, 100:]
+        log_sigma = q_params[:, :100]
+        z = self.reparameterize(mu, log_sigma)
+        return self.decoder(z), mu, log_sigma
+
+    def reparameterize(self, mu, log_sigma):
+        sigma = torch.exp(log_sigma) + math.exp(-7)
+
+        e = torch.randn_like(sigma)
+        return mu + sigma * e
+
+
+def ELBOWLoss(x, x_, mu, log_sigma):
+    KL = KLDivergence(mu, log_sigma)
+
+    BCE = nn.BCELoss(reduction="sum")
+    logpx_z = BCE(x_, x)
+    return (logpx_z + KL) #batch mean loss computed in BCE
+
+def KLDivergence(mu, log_sigma):
+    return -0.5 * torch.sum(1 + log_sigma - mu.pow(2) - log_sigma.exp())
+
+# Configure data loader
+image_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((.5, .5, .5),
+                         (.5, .5, .5))
+])
+
+def get_data_loader(dataset_location, batch_size):
+    trainvalid = torchvision.datasets.SVHN(
+        dataset_location, split='train',
+        download=True,
+        transform=image_transform
+    )
+
+    trainset_size = int(len(trainvalid) * 0.9)
+    trainset, validset = dataset.random_split(
+        trainvalid,
+        [trainset_size, len(trainvalid) - trainset_size]
+    )
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=0
+    )
+
+    validloader = torch.utils.data.DataLoader(
+        validset,
+        batch_size=batch_size,
+    )
+
+    testloader = torch.utils.data.DataLoader(
+        torchvision.datasets.SVHN(
+            dataset_location, split='test',
+            download=True,
+            transform=image_transform
+        ),
+        batch_size=batch_size,
+    )
+
+    return trainloader, validloader, testloader
+
+def train(VAE, train_loader, optimizer, device):
+    VAE.train()
+    losses = []
+    mean_loss = 0
+    total = 0
+    total_step = len(train_loader)
+    for batch_id, batchX in enumerate(train_loader):
+        batchX = batchX.to(device)
+        optimizer.zero_grad()
+        batchX_, mu, log_sigma = VAE(batchX)
+        loss = ELBOWLoss(batchX.view(-1,1024), batchX_.view(-1,1024), mu, log_sigma)
+        loss.backward()
+        losses.append(loss.item())
+        optimizer.step()
+        mean_loss += loss.item()
+        total += batchX.shape[0]
+
+        if (batch_id + 1) % 100 == 0:
+            print('Step [{}/{}], Loss: {:.4f}({:.4f})'
+                  .format(batch_id + 1, total_step, loss.item()/batchX.shape[0], mean_loss / total))
+
+    mean_loss = mean_loss / total
+    return mean_loss
+
+
+def validate(VAE, validation_loader, device):
+    VAE.eval()
+    mean_loss = 0
+    total = 0
+    with torch.no_grad():
+        for batch_id, batchX in enumerate(validation_loader):
+            batchX = batchX.to(device)
+            batchX_, mu, log_sigma = VAE(batchX)
+            loss = ELBOWLoss(batchX.view(-1,1024), batchX_.view(-1,1024), mu, log_sigma)
+            mean_loss += loss.item()
+            total += batchX.shape[0]
+
+        mean_loss = mean_loss / total
+        print('Validation loss: {:.4f}'.format(mean_loss))
+    return mean_loss
+
+
+if __name__ == "__main__":
+
+    device = torch.device("cpu")
+    if cuda.is_available():
+        device = torch.device("cuda")
+
+    train_loader, valid_loader, test_loader = get_data_loader("svhn", 32)
+
+    VAE = VAE()
+    VAE.to(device)
+    optimizer = Adam(params=VAE.parameters(), lr=3*10**(-4))
+    num_epochs = 20
+    trainLosses = []
+    validLosses = []
+    for epoch in range(num_epochs):
+        print("-------------- Epoch # " + str(epoch+1) + " --------------")
+
+        trainLoss = train(VAE, train_loader, optimizer, device)
+        trainLosses.append(trainLoss)
+        print("Epoch train loss: {:.4f}".format(trainLoss))
+
+        validationLoss = validate(VAE, valid_loader, device)
+        validLosses.append(validationLoss)
+
+        save_model(VAE, optimizer, epoch, trainLoss, validationLoss)
+
+    plt.plot(np.arange(num_epochs), trainLosses)
+    plt.plot(np.arange(num_epochs), validLosses)
+    plt.legend(["Training","Validation"])
+    plt.ylabel("ELBO Loss")
+    plt.xlabel("Epoch number")
+    plt.savefig("./results/VAE_training_20_epochs.png")
+    plt.show()
--- HW3/wgangp.py
+++ HW3/wgangp.py
@@ -13,24 +13,25 @@ import torchvision.transforms as transforms
 from torchvision.utils import save_image
 
 from torch.autograd import Variable
-
-import torch.nn as nn
+from torch import nn
 import torch.nn.functional as F
 import torch.autograd as autograd
 import torch
 import torchvision.datasets
 from torch.utils.data import dataset
-from torch import nn
 # from torch.nn.modules import upsampling
 # from torch.functional import F
 from torch.optim import Adam
 
-os.makedirs("images", exist_ok=True)
 
+# setup
+os.makedirs("images", exist_ok=True)
 
 img_shape = (3,32,32)
 
 cuda = True if torch.cuda.is_available() else False
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
 
 class Squeeze(nn.Module):
     def __init__(self, *args):
@@ -100,26 +101,8 @@ class Discriminator(nn.Module):
         return validity
 
 
-# Loss weight for gradient penalty
-lambda_gp = 10
 
-# Initialize generator and discriminator
-generator = Generator()
-discriminator = Discriminator()
-
-if cuda:
-    generator.cuda()
-    discriminator.cuda()
-
-# Configure data loader
-image_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((.5, .5, .5),
-                         (.5, .5, .5))
-])
-
-
-def get_data_loader(dataset_location, batch_size):
+def get_data_loader(dataset_location, batch_size, image_transform):
     trainvalid = torchvision.datasets.SVHN(
         dataset_location, split='train',
         download=True,
@@ -156,12 +139,6 @@ def get_data_loader(dataset_location, batch_size):
     return trainloader, validloader, testloader
 
 
-# Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5,0.999))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-
-Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
-
 
 def compute_gradient_penalty(D, real_samples, fake_samples):
     """Calculates the gradient penalty loss for WGAN GP"""
@@ -185,70 +162,3 @@ def compute_gradient_penalty(D, real_samples, fake_samples):
     return gradient_penalty
 
 
-# ----------
-#  Training
-# ----------
-n_epochs=50
-n_critic=5
-latent_dim=100
-sample_interval=400
-train, valid, test = get_data_loader("svhn", 32)
-batches_done = 0
-for epoch in range(n_epochs):
-    for i, (imgs, _) in enumerate(train):
-
-        # Configure input
-        real_imgs = Variable(imgs.type(Tensor))
-
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
-
-        optimizer_D.zero_grad()
-
-        # Sample noise as generator input
-        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], latent_dim))))
-
-        # Generate a batch of images
-        fake_imgs = generator(z)
-
-        # Real images
-        real_validity = discriminator(real_imgs)
-        # Fake images
-        fake_validity = discriminator(fake_imgs)
-        # Gradient penalty
-        gradient_penalty = compute_gradient_penalty(discriminator, real_imgs.data, fake_imgs.data)
-        # Adversarial loss
-        d_loss = -torch.mean(real_validity) + torch.mean(fake_validity) + lambda_gp * gradient_penalty
-
-        d_loss.backward()
-        optimizer_D.step()
-
-        optimizer_G.zero_grad()
-
-        # Train the generator every n_critic steps
-        if i % n_critic == 0:
-
-            # -----------------
-            #  Train Generator
-            # -----------------
-
-            # Generate a batch of images
-            fake_imgs = generator(z)
-            # Loss measures generator's ability to fool the discriminator
-            # Train on fake images
-            fake_validity = discriminator(fake_imgs)
-            g_loss = -torch.mean(fake_validity)
-
-            g_loss.backward()
-            optimizer_G.step()
-
-            print(
-                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-                % (epoch, n_epochs, i, len(train), d_loss.item(), g_loss.item())
-            )
-            
-            if batches_done % sample_interval == 0:
-                save_image(fake_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
-
-            batches_done += n_critic
