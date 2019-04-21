09948a20c43c030dbf482154414bc9291e337a86
Philippe <phlip.marcotte@gmail.com>
2019-04-21 16:16:51 -0400

    Importance sampling using matrices

--- HW3/VAE.py
+++ HW3/VAE.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn, cuda
 from torch.optim import Adam
 
-from Utils import save_model
+from Utils import save_model, load_model
 from mnist_loader import get_data_loader
 from tqdm import tqdm
 
@@ -61,42 +61,45 @@ class VAE(nn.Module):
         log_sigma, mu, z = self.sample_latent_from(x)
         return self.decoder(z), mu, log_sigma
 
-    def sample_latent_from(self, x):
+    def sample_latent_from(self, x, n_samples=1):
         q_params = self.encoder(x)
         mu = q_params[:, 100:]
         log_sigma = q_params[:, :100]
-        z = self.reparameterize(mu, log_sigma)
+        z = self.reparameterize(mu, log_sigma, n_samples)
         return log_sigma, mu, z
 
-    def reparameterize(self, mu, log_sigma):
+    def reparameterize(self, mu, log_sigma, n_samples=1):
         sigma = torch.exp(log_sigma) + math.exp(-7)
+        sigmas = sigma.unsqueeze(1).expand(-1, n_samples, -1)
+        mus = mu.unsqueeze(1).expand(-1, n_samples, -1)
 
-        e = torch.randn_like(sigma)
-        return mu + sigma * e
+        e = torch.randn((sigma.shape[0], n_samples, sigma.shape[1])).to(sigma.device)
+        return (mus + e * sigmas).squeeze()
 
     def importance_sampling(self, x, k):
-        Pis = []
-        for i in range(k):
-            log_sigma, mu, z = self.sample_latent_from(x)
-            x_ = self.decoder(z)
-            Pis.append(self.getPx_z(x_, x) + self.getPz(z) - self.getQz_x(z, mu, torch.exp(log_sigma)))
+        log_sigma, mu, z = self.sample_latent_from(x, n_samples=k)
+        log_sigma = log_sigma.unsqueeze(1).expand(-1, k, -1)
+        mu = mu.unsqueeze(1).expand(-1, k, -1)
+        x_ = self.decoder(z.view(-1, z.shape[-1])).view(-1, k, *x.shape[-3:])
+        P = self.getPx_z(x_, x.unsqueeze(1).expand(-1, k, *x.shape[-3:])) \
+            + self.getPz(z) - self.getQz_x(z, mu, torch.exp(log_sigma))
 
-        Pis = torch.stack(Pis).transpose(0, 1)
-        max,_ = torch.max(Pis, 1)
-        return max + torch.logsumexp(Pis - max.unsqueeze(1), 1) - np.log(k)
+        max, _ = torch.max(P, 1)
+        return max + torch.logsumexp(P - max.unsqueeze(1), 1) - np.log(k)
 
     def getPz(self, z):
         covar = torch.eye(100, 100).to(z.device)
         mu = torch.zeros(100).to(z.device)
-        return torch.distributions.multivariate_normal.MultivariateNormal(mu, covar).log_prob(z)
+        return torch.distributions.multivariate_normal.MultivariateNormal(mu, covar).log_prob(z.view(-1, z.shape[-1])).view(z.shape[0], z.shape[1])
 
     def getQz_x(self, z, mu, sigma):
-        covar = torch.diag_embed(sigma.unsqueeze(1)).squeeze()
+        covar = torch.diag_embed(sigma)
+
         return torch.distributions.multivariate_normal.MultivariateNormal(mu, covar).log_prob(z)
 
     def getPx_z(self, x_, x):
         BCE = nn.BCELoss(reduction="none")
-        return -BCE(x_.view(-1, 784), x.view(-1, 784)).sum(1)
+        return -BCE(x_, x).view(*x.shape[:2], -1).sum(-1)
 
 def ELBOWLoss(x, x_, mu, log_sigma):
     KL = KLDivergence(mu, log_sigma)
@@ -148,19 +151,23 @@ def validate(model, validation_loader, device):
         #print('Validation loss: {:.4f}'.format(mean_loss))
     return mean_loss
 
-def importance_sampling(model, data_loader, device):
+def importance_sampling(model, data_loader, device, k=200):
     model.eval()
 
     with torch.no_grad():
         importance_samples = []
-        for batch_id, batchX in enumerate(data_loader):
+        for batch_id, batchX in tqdm(enumerate(data_loader),
+                                     total=len(data_loader)):
             batchX = batchX.to(device)
-            approximate_per_batch.append(torch.mean(model.importance_sampling(batchX, 200)))
+            importance_samples.append(
+                model.importance_sampling(batchX, k).mean().detach().cpu())
 
-        return torch.mean(torch.stack(approximate_per_batch))
+        return torch.stack(importance_samples).mean()
 
 if __name__ == "__main__":
-
+    np.random.seed(0)
+    torch.cuda.manual_seed_all(0)
+    torch.manual_seed(0)
     device = torch.device("cpu")
     if cuda.is_available():
         device = torch.device("cuda")
@@ -168,13 +175,15 @@ if __name__ == "__main__":
     train_loader, valid_loader, test_loader = get_data_loader("binarized_mnist", 64)
 
     model = VAE()
+    checkpoint = None
+    # checkpoint = load_model()
+    # model.load_state_dict(checkpoint["model_state_dict"])
     model.to(device)
     optimizer = Adam(params=model.parameters(), lr=3 * 10 ** (-4))
     num_epochs = 20
     trainLosses = []
     validLosses = []
-    caca = importance_sampling(model, test_loader, device)
-    for epoch in range(num_epochs):
+    for epoch in range(checkpoint["epoch"] if checkpoint else 0, num_epochs):
         print("-------------- Epoch # " + str(epoch+1) + " --------------")
 
         trainLoss = train(model, train_loader, optimizer, device)
